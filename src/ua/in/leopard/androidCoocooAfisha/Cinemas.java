977e3d81ac91f10c8a3a92394bf1906460e2f423
Alexey <leopard.not.a@gmail.com>
2010-08-14 16:15:57 +0300

    Button

--- src/ua/in/leopard/androidCoocooAfisha/Cinemas.java
+++ src/ua/in/leopard/androidCoocooAfisha/Cinemas.java
@@ -1,6 +1,7 @@
 package ua.in.leopard.androidCoocooAfisha;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,12 +13,19 @@ public class Cinemas extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.cinemas);
 		
+        View backButton = findViewById(R.id.home_button);
+        backButton.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		
+		Intent i;
+		switch (v.getId()) {
+		  case R.id.home_button:
+	         i = new Intent(this, androidCoocooAfisha.class);
+	         startActivity(i);
+	         break;     
+	      }
 	}
 
 }
