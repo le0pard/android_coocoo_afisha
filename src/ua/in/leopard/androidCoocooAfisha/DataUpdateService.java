06c55b40911268208fe6d7c5a6a37a7aca41fd67
Alexey <leopard.not.a@gmail.com>
2011-02-22 22:29:41 +0200

    Fix timer

--- src/ua/in/leopard/androidCoocooAfisha/DataUpdateService.java
+++ src/ua/in/leopard/androidCoocooAfisha/DataUpdateService.java
@@ -6,14 +6,12 @@ import java.util.TimerTask;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-/*import android.util.Log;*/
 import android.widget.Toast;
 
 public class DataUpdateService extends Service {
 	//private static final String TAG = "DataUpdateService";
 	private Timer timer = new Timer();
 	private int hours_interval = 1;
-	private Boolean first_iteration_cycle = true;
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -25,7 +23,6 @@ public class DataUpdateService extends Service {
 		//Toast.makeText(this, "My Service Created", Toast.LENGTH_LONG).show();
 		//Log.d(TAG, "DataUpdateService onCreate");
 		this.hours_interval = Integer.parseInt(EditPreferences.getAutoUpdateTime(this));
-		this.first_iteration_cycle = true;
 		startTimer();
 	}
 
@@ -45,24 +42,16 @@ public class DataUpdateService extends Service {
 	}
 	
 	private void startTimer(){
+		int timeout = 1000 * 3600 * this.hours_interval;
 		timer.scheduleAtFixedRate(
 			new TimerTask() {
 				public void run() {
-					updateData();
+					DataCollector dataCollectorObject = new DataCollector(getApplicationContext());
+					dataCollectorObject.getTheatersData();
+					dataCollectorObject.getCinemasData();
 				}
 			}, 
-		0, 1000 * 3600 * this.hours_interval);
-	}
-	
-	private void updateData(){
-		if (first_iteration_cycle){
-			first_iteration_cycle = false;
-		} else {
-			DataCollector dataCollectorObject = new DataCollector(getApplicationContext());
-			dataCollectorObject.getTheatersData();
-			dataCollectorObject.getCinemasData();
-			//Log.d(TAG, "dataUpdated");
-		}
+			timeout, timeout);
 	}
 
 }
