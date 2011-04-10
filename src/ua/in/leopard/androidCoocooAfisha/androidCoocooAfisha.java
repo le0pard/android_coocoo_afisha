ec8e597b958c1b3e43ac0d71a42f79384abc6bd7
Alexey <leopard.not.a@gmail.com>
2011-04-11 00:00:12 +0300

    Begin top and add styles

--- src/ua/in/leopard/androidCoocooAfisha/androidCoocooAfisha.java
+++ src/ua/in/leopard/androidCoocooAfisha/androidCoocooAfisha.java
@@ -23,8 +23,6 @@ public class androidCoocooAfisha extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        setTitle(getString(R.string.main_title));
-        
         View cinemasButton = findViewById(R.id.cinemas_button);
         cinemasButton.setOnClickListener(this);
         View theatersButton = findViewById(R.id.theaters_button);
