ed7209eedea535624b0a0280f81a5b3d2ffbfec3
Alexey <leopard.not.a@gmail.com>
2011-05-03 23:24:48 +0300

    Fix map label

--- src/ua/in/leopard/androidCoocooAfisha/TheatersItemizedOverlay.java
+++ src/ua/in/leopard/androidCoocooAfisha/TheatersItemizedOverlay.java
@@ -83,7 +83,8 @@ public class TheatersItemizedOverlay extends ItemizedOverlay<TheaterOverlayItem>
 				mapView.getProjection().toPixels(selected_item.getPoint(), selDestinationOffset);
 				//  Setup the info window with the right size & location
 				Paint p = new Paint();
-				float text_width = p.measureText(selected_item.getTheaterObj().getTitle()) + 20;
+				String theater_title = Html.fromHtml(selected_item.getTheaterObj().getTitle()).toString();
+				float text_width = p.measureText(theater_title) + 20;
 				int INFO_WINDOW_WIDTH = (int)(text_width * mGestureThreshold + 0.5f);
 				int INFO_WINDOW_HEIGHT = (int)(25 * mGestureThreshold + 0.5f);
 				RectF infoWindowRect = new RectF(0, 0, INFO_WINDOW_WIDTH, INFO_WINDOW_HEIGHT);				
@@ -97,7 +98,7 @@ public class TheatersItemizedOverlay extends ItemizedOverlay<TheaterOverlayItem>
 				//  Draw the MapLocation's name
 				int TEXT_OFFSET_X = (int)(10 * mGestureThreshold + 0.5f);
 				int TEXT_OFFSET_Y = (int)(16 * mGestureThreshold + 0.5f);
-				canvas.drawText(selected_item.getTheaterObj().getTitle(),
+				canvas.drawText(theater_title,
 						infoWindowOffsetX + TEXT_OFFSET_X,
 						infoWindowOffsetY + TEXT_OFFSET_Y, 
 						getTextPaint());
